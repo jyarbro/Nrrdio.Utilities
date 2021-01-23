3892143e6db7093f9f70d8470e6875349211effb
Jim Yarbro <nrrdio@outlook.com>
2021-01-23 12:23:17 +0100

    Default section title

--- Nrrdio.Utilities.Web/Models/Options/GzipWebClientOptions.cs
+++ Nrrdio.Utilities.Web/Models/Options/GzipWebClientOptions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Nrrdio.Utilities.Web.Models.Options {
     public class GzipWebClientOptions {
+        public const string Section = "GzipWebClient";
+
         public string UserAgent { get; set; }
         public int Timeout { get; set; }
     }
