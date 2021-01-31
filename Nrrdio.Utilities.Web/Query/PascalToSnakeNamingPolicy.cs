2734b95c0e388d1a3f516acea33d22bb9dfa00c5
Jim Yarbro <nrrdio@outlook.com>
2021-01-31 10:11:00 +0100

    Added

---
+++ Nrrdio.Utilities.Web/Query/PascalToSnakeNamingPolicy.cs
@@ -0,0 +1,12 @@
+﻿using Nrrdio.Utilities.Text;
+using System.Text.Json;
+
+namespace Nrrdio.Utilities.Web.Query {
+    public class PascalToSnakeNamingPolicy : JsonNamingPolicy {
+        public override string ConvertName(string name) => name.PascalToSnake();
+
+        public static JsonSerializerOptions Options => new JsonSerializerOptions {
+            PropertyNamingPolicy = new PascalToSnakeNamingPolicy()
+        };
+    }
+}
---
+++ Nrrdio.Utilities.Web/Query/QuerySerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Web;
+
+namespace Nrrdio.Utilities.Web.Query {
+    public class QuerySerializer {
+        /// <summary>
+        /// Converts a simple object into a query string.
+        /// </summary>
+        /// <param name="namingPolicy">Used especially to format the names of properties.</param>
+        public static string Serialize(object obj, JsonNamingPolicy namingPolicy = null) {
+            JsonSerializerOptions options = null;
+
+            if (namingPolicy is not null) {
+                options = new JsonSerializerOptions { PropertyNamingPolicy = namingPolicy };
+            }
+
+            var serialized = JsonSerializer.Serialize(obj, options);
+            var deserialized = JsonSerializer.Deserialize<IDictionary<string, object>>(serialized);
+            var query = deserialized.Select(o => $"{HttpUtility.UrlEncode(o.Key)}={HttpUtility.UrlEncode(o.Value.ToString())}");
+
+            return string.Join("&", query);
+        }
+    }
+}
