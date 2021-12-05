b55caeb7fb5a03d331279eeb5cb8b20c2e0b5213
Jim Yarbro <yarbro@outlook.com>
2021-12-05 10:38:32 -0500

    Add WinUI utilities project.

---
+++ Nrrdio.Utilities.WinUI/BooleanToVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Nrrdio.Utilities.WinUI {
+    public class BooleanToVisibilityConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value is bool castedValue
+               && castedValue ? Visibility.Visible : (object)Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => value is Visibility castedValue
+               && castedValue == Visibility.Visible;
+    }
+}
