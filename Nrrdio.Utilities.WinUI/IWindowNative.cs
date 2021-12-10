6c36c2c3bd8aab22a890f1657d0317e42a6242c6
Jim Yarbro <yarbro@outlook.com>
2021-12-09 22:13:38 -0500

    namespace, duh.

--- Nrrdio.Utilities.WinUI/IWindowNative.cs
+++ Nrrdio.Utilities.WinUI/IWindowNative.cs
@@ -2,9 +2,11 @@
 using System.Runtime.InteropServices;
 
 // https://github.com/microsoft/WinUI-3-Demos/blob/420c48fe1613cb20b38000252369a0c556543eac/src/Build2020Demo/DemoBuildCs/DemoBuildCs/DemoBuildCs/App.xaml.cs#L60
-[ComImport]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-[Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
-internal interface IWindowNative {
-    IntPtr WindowHandle { get; }
+namespace Nrrdio.Utilities.WinUI {
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+    internal interface IWindowNative {
+        IntPtr WindowHandle { get; }
+    }
 }
