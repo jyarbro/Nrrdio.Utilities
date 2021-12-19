fc38469e98c0df56a69acaae87700a68eee8617c
Jim Yarbro <yarbro@outlook.com>
2021-12-19 11:25:14 -0500

    added inverse testing, found flaws.

--- Nrrdio.Utilities.Tests/Maths/Segments.cs
+++ Nrrdio.Utilities.Tests/Maths/Segments.cs
@@ -21,26 +21,26 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(1, 4), new Point(4, 1));
 
-            var intersection = (true, new Point(3, 2), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
-        public void NoIntersectFromNegative() {
+        public void NoIntersect() {
             var segment1 = new Segment(new Point(1, -2), new Point(2.5, 1));
             var segment2 = new Segment(new Point(1, 4), new Point(4, 1));
 
-            var intersection = (false, default(Point), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
         public void LargeNoIntersect() {
-            var segment1 = new Segment(new Point(908.66463272924955, 300), new Point(774.96302369625346, 328.9112985440172));
+            var segment1 = new Segment(new Point(-113.68370987427628, 300), new Point(-59.179092628201211, 276.83033152363953));
             var segment2 = new Segment(new Point(0, 0), new Point(0, 600));
 
-            var intersection = (false, default(Point), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -48,8 +48,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(1, 4), new Point(3, 2));
 
-            var intersection = (true, new Point(3, 2), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -57,8 +57,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(0, 2), new Point(4, 2));
 
-            var intersection = (true, new Point(3, 2), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -66,8 +66,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(3, 4), new Point(3, 0));
 
-            var intersection = (true, new Point(3, 2), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((true, new Point(3, 2), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -75,8 +75,17 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(3, 2), new Point(3.5, 3));
 
-            var intersection = (true, segment2.Point1, segment2.Point2);
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point1, segment2.Point2), segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point1, segment2.Point2), segment2.Intersects(segment1));
+        }
+
+        [TestMethod]
+        public void ColinearReverseOverlap() {
+            var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
+            var segment2 = new Segment(new Point(3.5, 3), new Point(3, 2));
+
+            Assert.AreEqual((true, segment2.Point1, segment2.Point2), segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point2, segment2.Point1), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -84,17 +93,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(5, 6), new Point(6, 8));
 
-            var intersection = (false, default(Point), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
-        }
-
-        [TestMethod]
-        public void OverlapsReverse() {
-            var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
-            var segment2 = new Segment(new Point(3.5, 3), new Point(3, 2));
-
-            var intersection = (true, segment2.Point1, segment2.Point2);
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -102,8 +102,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(3, 2), new Point(5, 6));
 
-            var intersection = (true, segment2.Point1, segment1.Point2);
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point1, segment1.Point2), segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point1, segment1.Point2), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -111,8 +111,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(5, 6), new Point(3, 2));
 
-            var intersection = (true, segment1.Point2, segment2.Point2);
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment1.Point2, segment2.Point2), segment1.Intersects(segment2));
+            Assert.AreEqual((true, segment2.Point2, segment1.Point2), segment2.Intersects(segment1));
         }
 
         [TestMethod]
@@ -120,8 +120,8 @@ namespace Nrrdio.Utilities.Tests {
             var segment1 = new Segment(new Point(2, 0), new Point(4, 4));
             var segment2 = new Segment(new Point(2, 2), new Point(4, 6));
 
-            var intersection = (false, default(Point), default(Point));
-            Assert.AreEqual(intersection, segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment1.Intersects(segment2));
+            Assert.AreEqual((false, default(Point), default(Point)), segment2.Intersects(segment1));
         }
 
         [TestMethod]
