307c3c083e33318cc411a73b02f03099fa53627f
Jim Yarbro <yarbro@outlook.com>
2023-03-14 09:54:34 -0400

    Added new http error

---
+++ Nrrdio.Utilities.Web/Models/Errors/HttpTooManyRequestsError.cs
@@ -0,0 +1,7 @@
+﻿namespace Nrrdio.Utilities.Web.Models.Errors;
+
+public class HttpTooManyRequestsError : HttpException {
+	public override int StatusCode => 429;
+
+	public HttpTooManyRequestsError() : base("Either you are sending requests too quickly or the servers are experiencing high traffic.") { }
+}
