bd05ef317dd7d964208a9fca6beda611ef27e808
Jim Yarbro <yarbro@outlook.com>
2023-08-28 12:14:45 -0400

    Added a wait helper

---
+++ Nrrdio.Utilities/Wait.cs
@@ -0,0 +1,18 @@
+﻿namespace Nrrdio.Utilities;
+
+public class Wait {
+    public bool Continue { get; set; }
+
+    public async Task ForContinue() {
+        Continue = false;
+
+        await Task.Run(() => {
+            while (!Continue) {
+                // 80ms after a click feels pretty snappy but doesn't bog down the machine with a loop.
+                Thread.Sleep(80);
+            }
+        });
+    }
+
+    public async Task For(int ms) => await Task.Delay(ms);
+}
