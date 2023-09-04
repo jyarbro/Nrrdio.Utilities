496d3fcd985b3bebf8fafafe1617cb4b7b3c0e4c
Jim Yarbro <yarbro@outlook.com>
2023-09-04 19:52:13 -0400

    Added some new formulas

---
+++ Nrrdio.Utilities.Maths.Tests/Formulas.cs
@@ -0,0 +1,10 @@
+﻿namespace Nrrdio.Utilities.Maths.Tests;
+
+[TestClass]
+public class Formulas {
+    [TestMethod]
+    public void Lerp() {
+        Assert.AreEqual(2, Formula.Lerp(1, 3, 0.5));
+        Assert.AreEqual(4, Formula.Lerp(1, 3, 1.5));
+    }
+}
