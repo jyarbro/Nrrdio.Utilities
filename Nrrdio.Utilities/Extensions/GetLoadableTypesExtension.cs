230f2f3fc0502469f61ff09dbe0c8bb29e51932e
Jim Yarbro <yarbro@outlook.com>
2024-01-16 12:38:03 -0500

    Consolidated multiple extension classes into new namespace. Added loadable types extension

---
+++ Nrrdio.Utilities/Extensions/GetLoadableTypesExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Nrrdio.Utilities.Extensions;
+
+public static class GetLoadableTypesExtension {
+	// Original: https://haacked.com/archive/2012/07/23/get-all-types-in-an-assembly.aspx/
+	public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly) {
+		ArgumentNullException.ThrowIfNull(assembly);
+
+		try {
+			return assembly.GetTypes();
+		}
+		catch (ReflectionTypeLoadException e) {
+			return e.Types.Where(t => t is not null).Cast<Type>();
+		}
+	}
+}
